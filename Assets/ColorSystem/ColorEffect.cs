0e2a765528c6fe1bcd72c3a9d9916326e9e815cc
EmilEkroth <emil@ekroth.se>
2023-03-28 19:31:37 +0200

    Color buff and comments
    Added the color buff
    Added comments to the ColorInventory

--- Assets/ColorSystem/ColorEffect.cs
+++ Assets/ColorSystem/ColorEffect.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ColorEffect", menuName = "Gameplay Color/Color Effect/ColorEffect")]
 public abstract class ColorEffect : ScriptableObject
 {
-    public abstract void Apply(GameObject enemy, GameObject player);
+    public abstract void Apply(GameObject enemy, GameObject player, float power);
 }
--- Assets/ColorSystem/ColorInventory.cs
+++ Assets/ColorSystem/ColorInventory.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Keeps track of the colors that the player has gathered. 
+/// </summary>
 public class ColorInventory : MonoBehaviour
 {
     int startColorSlots;
+    [SerializeField] float colorBuff;
     [SerializeField] List<ColorSlot> colorSlots;
-    
     [SerializeField] int activeSlot;
     [SerializeField] InputActionReference changeRightActions;
     [SerializeField] Image[] images;
@@ -33,12 +36,20 @@ public class ColorInventory : MonoBehaviour
         changeRightActions.action.performed -= (dir) => {RotateActive((int)dir.ReadValue<float>()); };
     }
 
+    /// <summary>
+    /// Changes the active color slot by rotating in a direction
+    /// </summary>
+    /// <param name="dir"></param>
     public void RotateActive(int dir)
     {
         activeSlot = (colorSlots.Count+activeSlot+dir)%colorSlots.Count;
         Debug.Log(ActiveSlot().gameColor);
     }
 
+    /// <summary>
+    /// Returns the color effect from the active color slot
+    /// </summary>
+    /// <returns></returns>
     public ColorEffect UseActiveColorEffect()
     {
         if(ActiveSlot().charge > 0)
@@ -50,6 +61,38 @@ public class ColorInventory : MonoBehaviour
         return null;
     }
 
+    /// <summary>
+    /// Returns the color buff of the specified color. Returns 1 if no color buff exists.
+    /// </summary>
+    /// <param name="color"></param>
+    /// <returns></returns>
+    public float GetColorBuff(GameColor color)
+    {
+        foreach (ColorSlot slot in colorSlots)
+        {
+            if(slot.gameColor == color && slot.charge == slot.maxCapacity) 
+            {
+                return colorBuff;
+            }
+        }
+
+        return 0;
+    }
+    
+    /// <summary>
+    /// Gets the color buff for the active slots color- Returns 1 if no color buff exists.
+    /// </summary>
+    /// <returns></returns>
+    public float GetColorBuff()
+    {
+        return GetColorBuff(ActiveSlot().gameColor);
+    }
+
+    /// <summary>
+    /// Adds color to the active color slott. Mixes the colors if color already exist there
+    /// </summary>
+    /// <param name="color"></param>
+    /// <param name="amount"></param>
     public void AddColor(GameColor color, int amount)
     {
         GameColor setColor;
@@ -64,6 +107,9 @@ public class ColorInventory : MonoBehaviour
         colorSlots[activeSlot].SetGameColor(setColor);
     }
 
+    /// <summary>
+    /// Remove all color from all color slots
+    /// </summary>
     public void RemoveAllColors()
     {
         foreach (ColorSlot item in colorSlots)
@@ -72,22 +118,35 @@ public class ColorInventory : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Removes the color from the active color slot
+    /// </summary>
     public void RemoveActiveColor()
     {
         ActiveSlot().RemoveColor();
     }
 
+    /// <summary>
+    /// Returns the active color slot
+    /// </summary>
+    /// <returns></returns>
     private ColorSlot ActiveSlot()
     {
         return colorSlots[activeSlot];
     }
 
+    /// <summary>
+    /// Adds a color slot. Can't add more color slots than the UI has specified
+    /// </summary>
     public void AddColorSlot()
     {
         colorSlots.Add(new ColorSlot());
         colorSlots[colorSlots.Count-1].Init(images[colorSlots.Count-1]);
     }
 
+    /// <summary>
+    /// Removes the last color slot
+    /// </summary>
     public void RemoveColorSlot()
     {
         colorSlots.RemoveAt(colorSlots.Count-1);
@@ -96,6 +155,9 @@ public class ColorInventory : MonoBehaviour
         images[colorSlots.Count].transform.parent.gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// Removes all color and resets the ammount of slots to the initial state.
+    /// </summary>
     public void ResetColorSlots()
     {
         RemoveAllColors();
@@ -104,6 +166,9 @@ public class ColorInventory : MonoBehaviour
     }
 }
 
+/// <summary>
+/// Defines the traits of a color slot
+/// </summary>
 [System.Serializable]
 public struct ColorSlot
 {
