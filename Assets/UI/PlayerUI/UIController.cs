5ca12d97f1aabd2e51dfe7e4df75dd2b06371681
Samuel Dahlborg <beaosam@gmail.com>
2023-04-14 17:04:22 +0200

    Reworked the colorSlot system in UI

--- Assets/UI/PlayerUI/UIController.cs
+++ Assets/UI/PlayerUI/UIController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private ColorInventory colorInventory;
+    [SerializeField] List<GameObject> colorSlotContainers;
+
+    private void OnEnable() {
+        colorInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<ColorInventory>();
+        colorInventory.onColorSlotsChanged += colorSlotUpdate;
+        colorSlotUpdate();
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDisable() {
+        colorInventory.onColorSlotsChanged -= colorSlotUpdate;
+    }
+
+
+    private void colorSlotUpdate() {
+        foreach(GameObject colorSlotContainer in colorSlotContainers) {
+            colorSlotContainer.SetActive(false);
+        }
+
+        if(colorInventory.colorSlots.Count == 3) {colorSlotContainers[0].SetActive(true);}
+        if(colorInventory.colorSlots.Count == 4) {colorSlotContainers[1].SetActive(true);}
+        if(colorInventory.colorSlots.Count == 5) {colorSlotContainers[2].SetActive(true);}
+        if(colorInventory.colorSlots.Count == 6) {colorSlotContainers[3].SetActive(true);}
     }
 }
