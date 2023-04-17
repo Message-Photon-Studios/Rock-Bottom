436307fc2d8982493b2809181689db260527208e
EmilEkroth <emil@ekroth.se>
2023-04-17 19:25:05 +0200

    Added  enemy behaviours

---
+++ Assets/Enemies/EnemyBehaviours/CheckPlayerBehind.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourTree;
+
+public class CheckPlayerBehind : Node
+{
+    Transform player;
+    EnemyStats stats;
+
+    public CheckPlayerBehind(EnemyStats stats, PlayerStats player)
+    {
+        this.stats = stats;
+        this.player = player.transform;
+    }
+
+    public override NodeState Evaluate()
+    {
+        state = ((player.position.x - stats.GetPosition().x)*stats.lookDir<0)? NodeState.SUCCESS:NodeState.FAILURE;
+        return state;
+    }
+}
---
+++ Assets/Enemies/EnemyBehaviours/RunForward.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourTree;
+
+public class RunForward : Node
+{
+    EnemyStats stats;
+    Rigidbody2D body;
+    float speedFactor;
+
+    public RunForward(EnemyStats stats, float speedFactor)
+    {
+        this.stats = stats;
+        this.body = stats.GetComponent<Rigidbody2D>();
+        this.speedFactor = speedFactor;
+    }
+
+    public override NodeState Evaluate()
+    {
+        body.AddForce(new Vector2(stats.lookDir * stats.GetSpeed() * speedFactor, 0f));
+        state = NodeState.SUCCESS;
+        return state; 
+    }
+}
