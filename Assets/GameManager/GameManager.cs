833c708fc5408aed55dc4f8fbaad876750ffdca2
EmilEkroth <emil@ekroth.se>
2023-04-18 22:37:27 +0200

    Added escape to main menu

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] LevelGenManager levelGenerator;
     [SerializeField] string nextLevelName;
+    [SerializeField] string mainMenuName;
+    [SerializeField] InputActionReference menuAction;
     private void Start() 
     {
         levelGenerator.init();
     }
 
+    void OnEnable()
+    {
+        menuAction.action.performed += LoadMainMenu;
+    }
+
+    void OnDisable()
+    {
+        menuAction.action.performed -= LoadMainMenu;
+    }
+
+    private void LoadMainMenu(InputAction.CallbackContext ctx)
+    {
+        SceneManager.LoadScene(mainMenuName);
+    }
+
     public void EndLevel()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(nextLevelName);
     }
 }
