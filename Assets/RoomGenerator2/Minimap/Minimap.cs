193307961bd35a7923315c57294a0265740f40ea
AsperTheDog <guillerman0000@gmail.com>
2023-04-20 19:03:57 +0200

    Minimap working

--- Assets/RoomGenerator2/Minimap/Minimap.cs
+++ Assets/RoomGenerator2/Minimap/Minimap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,15 +34,45 @@ public class Minimap
         var nodePos = new Vector2(
             (float)Math.Round(position.x / size), 
             (float)Math.Round(position.y / size));
+        
+        disoverTile(nodePos + new Vector2(-1,  1));
+        disoverTile(nodePos + new Vector2( 1, -1));
+        disoverTile(nodePos + new Vector2(-1, -1));
+        disoverTile(nodePos + new Vector2(-1,  0));
+        disoverTile(nodePos + new Vector2( 0, -1));
+        disoverTile(nodePos + new Vector2( 1,  1));
+        disoverTile(nodePos + new Vector2( 1,  0));
+        disoverTile(nodePos + new Vector2( 0,  1));
+        disoverTile(nodePos + new Vector2( 0,  0));
+    }
 
+    private void disoverTile(Vector2 nodePos)
+    {
+        var size = LevelGenManager.ROOMSIZE * 2;   
         if (!nodes.ContainsKey(nodePos) || nodes[nodePos].explored) 
             return;
 
         var basicRoom = Resources.Load("Minimap/RoomBasic");
+        var basicWall = Resources.Load("Minimap/WallBasic");
         nodes[nodePos].explored = true;
         var obj = (GameObject) UnityEngine.Object.Instantiate(basicRoom, nodePos * 2 * LevelGenManager.ROOMSIZE, Quaternion.identity);
         obj.name = nodePos + " room";
         obj.transform.parent = minimapHolder.transform.Find("minimapRooms");
+
+        var node = nodes[nodePos].node;
+        for (var i = 0; i < 4; i++)
+        {
+            if (node.doors[i])
+                continue;
+
+            var wall = (GameObject) UnityEngine.Object.Instantiate(
+            basicWall, 
+                nodePos * size + CustomRoom.dirVectors[i] * LevelGenManager.ROOMSIZE, 
+                Quaternion.identity);
+            wall.transform.parent = obj.transform;
+            if (i is (int)Direction.Down or (int)Direction.Up)
+                wall.transform.Rotate(new Vector3(0, 0, 1), 90);
+        }
     }
 
     private void generateElements()
