371b0384fb7b7beb99d0fe7187069ca49dda9233
AsperTheDog <guillerman0000@gmail.com>
2023-04-20 17:38:16 +0200

    Fixed position detector and removed fog of war

--- Assets/RoomGenerator2/Minimap/Minimap.cs
+++ Assets/RoomGenerator2/Minimap/Minimap.cs
@@ -29,59 +29,29 @@ public class Minimap
 
     public void testPosition(Vector2 position)
     {
+        var size = LevelGenManager.ROOMSIZE * 2;
         var nodePos = new Vector2(
-            (position.x - (position.x % LevelGenManager.ROOMSIZE)) / LevelGenManager.ROOMSIZE, 
-            (position.y - (position.y % LevelGenManager.ROOMSIZE)) / LevelGenManager.ROOMSIZE);
+            (float)Math.Round(position.x / size), 
+            (float)Math.Round(position.y / size));
 
-        if (nodes.ContainsKey(nodePos) && !nodes[nodePos].explored)
-        {
-            nodes[nodePos].explored = true;
-            // Find and delete child with name nodePos + " black"
-            var child = minimapHolder.transform.Find("fogOfWar").Find(nodePos + " black");
-            UnityEngine.Object.DestroyImmediate(child.gameObject);
-        }
+        if (!nodes.ContainsKey(nodePos) || nodes[nodePos].explored) 
+            return;
+
+        var basicRoom = Resources.Load("Minimap/RoomBasic");
+        nodes[nodePos].explored = true;
+        var obj = (GameObject) UnityEngine.Object.Instantiate(basicRoom, nodePos * 2 * LevelGenManager.ROOMSIZE, Quaternion.identity);
+        obj.name = nodePos + " room";
+        obj.transform.parent = minimapHolder.transform.Find("minimapRooms");
     }
 
     private void generateElements()
     {
-        var basicRoom = Resources.Load("Minimap/RoomBasic");
-        var outOfBounds = Resources.Load("Minimap/OutOfBounds");
-        var wall = Resources.Load("Minimap/WallBasic");
-
         // Create a GameObject called Minimap
         minimapHolder = GameObject.Find("minimap");
         if (minimapHolder != null)
             UnityEngine.Object.DestroyImmediate(minimapHolder);
-        minimapHolder = new GameObject("minimap");
 
-        // Set minimapHolder layer to be minimap
-        minimapHolder.layer = LayerMask.NameToLayer("Minimap");
-        
-        var minimapRooms = new GameObject("minimapRooms");
-        minimapRooms.transform.parent = minimapHolder.transform;
-        foreach (var node in nodes)
-        {
-            //Instantiate a basic room in the position of the node
-            var obj = (GameObject) UnityEngine.Object.Instantiate(basicRoom, node.Key * 2 * LevelGenManager.ROOMSIZE, Quaternion.identity);
-            obj.name = node.Key + " room";
-            obj.transform.parent = minimapRooms.transform;
-        }
-
-        var minPos = new Vector2(nodes.Select(elem => elem.Key.x).Min() - 5, nodes.Select(elem => elem.Key.y).Min() - 5);
-        var maxPos = new Vector2(nodes.Select(elem => elem.Key.x).Max() + 5, nodes.Select(elem => elem.Key.y).Max() + 5);
-        
-        
-        var fogOfWar = new GameObject("fogOfWar");
-        fogOfWar.transform.parent = minimapHolder.transform;
-        for (var x = minPos.x; x < maxPos.x; x++)
-        {
-            for (var y = minPos.y; y < maxPos.y; y++)
-            {
-                var pos = new Vector2(x, y);
-                var obj = (GameObject) UnityEngine.Object.Instantiate(outOfBounds, pos * 2 * LevelGenManager.ROOMSIZE, Quaternion.identity);
-                obj.name = pos + " black";
-                obj.transform.parent = fogOfWar.transform;
-            }
-        }
+        minimapHolder = new GameObject("minimap") { layer = LayerMask.NameToLayer("Minimap") };
+        var minimapRooms = new GameObject("minimapRooms") { transform = { parent = minimapHolder.transform } };
     }
 }
