6616c1a685fbbc90068b8718f32d197445c800ee
hedy-99 <hedypet@hotmail.com>
2023-05-03 16:47:28 +0200

    walking, taking damage, and jumping sounds
    walking implemented, taking damage imlemented, jumping added, but still to be implemented

---
+++ Assets/sounds/player/PlayerSounds.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    public Animator player;
+
+    public AudioSource walking;
+    public AudioSource jumping;
+    public AudioSource climbing;
+
+    public AudioSource takingDamage;
+
+    private string walkingBool = "walking";
+    private string damageBool = "damaged";
+
+
+    public void Update()
+    {
+        if (player.GetBool(walkingBool))
+        {
+            PlayWalking();
+        }
+        else
+        {
+            StopWalking();
+        }
+
+        if (player.GetBool(damageBool)){
+            PlayTakingDamage();
+        }
+    }
+
+    // for each sound there is a play method
+    // for looping ones there needs to be a stop method
+
+    public void PlayWalking()
+    {
+        if (!walking.isPlaying)
+        {
+            walking.Play();
+        }
+        
+    }
+    public void StopWalking()
+    {
+        walking.Stop();
+    }
+
+    public void PlayTakingDamage()
+    {
+        if (!takingDamage.isPlaying)
+        {
+            takingDamage.Play();
+        }
+    }
+
+}
