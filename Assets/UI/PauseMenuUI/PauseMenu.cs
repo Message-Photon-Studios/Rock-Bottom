f80879ae855990f2e166b6c21c9cb8e119e6bdc9
leooooowei <weipeiran99922@gmail.com>
2023-05-04 12:49:59 +0200

    [Add] Pause menu set up & Initial button layout

---
+++ Assets/UI/PauseMenuUI/PauseMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+
+    }
+
+    public void ResumeGame()
+    {
+        
+    }
+}
