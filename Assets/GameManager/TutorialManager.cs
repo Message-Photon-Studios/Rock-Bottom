1e3208dfbb3157ed929695109ed9cc5c308b49de
EmilEkroth <emil@ekroth.se>
2023-05-10 14:05:55 +0200

    Added tutorial manager and dummy sounds

---
+++ Assets/GameManager/TutorialManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] GameObject dummyTemplate;
+    [SerializeField] GameObject[] dummys;
+
+    Vector3[] spawnPoints = new Vector3[0];
+
+    void Start()
+    {
+        spawnPoints = new Vector3[dummys.Length];
+        for (int i = 0; i < dummys.Length; i++)
+        {
+            spawnPoints[i] = dummys[i].transform.position;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < dummys.Length; i++)
+        {
+            if(!dummys[i])
+            {
+                GameObject newDummy = GameObject.Instantiate(dummyTemplate,spawnPoints[i], dummyTemplate.transform.rotation) as GameObject;
+                newDummy.GetComponent<EnemyStats>().SetColor(GetComponent<EnemyManager>().GetRandomEnemyColor());
+                dummys[i] = newDummy;
+            }
+        }
+    }
+}
