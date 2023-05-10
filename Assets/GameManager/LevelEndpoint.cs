2fd70af4f0aea2568cb3bc490d9ad2b271ad76bd
EmilEkroth <emil@ekroth.se>
2023-05-10 14:51:43 +0200

    Added tutorial exit

--- Assets/GameManager/LevelEndpoint.cs
+++ Assets/GameManager/LevelEndpoint.cs
@@ -1,14 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LevelEndpoint : MonoBehaviour
 {
+    [SerializeField] InputActionReference interactAction;
+    [SerializeField] GameObject canvas;
+    bool enableExit = false;
+
+    void OnEnable()
+    {
+        if(interactAction)
+            interactAction.action.performed += ExitLevel;
+    }
+
+    void OnDisable()
+    {
+        if(interactAction)
+            interactAction.action.performed -= ExitLevel;
+    }
+
+    void ExitLevel (InputAction.CallbackContext ctx)
+    {
+        if(!enableExit) return;
+        GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().EndLevel();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().EndLevel();
+            canvas.SetActive(true);
+            enableExit = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            canvas.SetActive(false);
+            enableExit = false;
         }
     }
 }
