739120852a1def3af8f79d5b862514088538d7f3
AsperTheDog <guillerman0000@gmail.com>
2023-05-16 15:52:15 +0200

    Health resizes when max health is increased

--- Assets/UI/PlayerUI/PlayerHpController.cs
+++ Assets/UI/PlayerUI/PlayerHpController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Time = UnityEngine.Time;
 
 public class PlayerHpController : MonoBehaviour
 {
@@ -9,6 +11,11 @@ public class PlayerHpController : MonoBehaviour
     PlayerStats playerStats;
     // The UI component for the health bar.
     [SerializeField] Slider healthSlider;
+    [SerializeField] AnimationCurve maxHpChangeCurve;
+ 
+    private RectTransform rect;
+    private float origSize = 1;
+    private float sizeMultiplier;
 
     private void OnEnable() {
         playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
@@ -16,8 +23,10 @@ public class PlayerHpController : MonoBehaviour
         playerStats.onHealthChanged += HpChanged;
         playerStats.onPlayerDied += PlayerDied;
         gameObject.SetActive(true);
-        MaxHpChanged(playerStats.GetMaxHealth());
+        //MaxHpChanged(playerStats.GetMaxHealth());
         HpChanged(playerStats.GetHealth());
+        rect = gameObject.GetComponent<RectTransform>();
+        origSize = rect.rect.width;
     }
 
     private void OnDisable() {
@@ -32,7 +41,13 @@ public class PlayerHpController : MonoBehaviour
     /// If current health is higher than max health, also update current value.
     /// </summary>
     /// <param name="newMaxHp"></param> Float with new max hp.
-    private void MaxHpChanged(float newMaxHp) {
+    private void MaxHpChanged(float newMaxHp)
+    {
+        if (sizeMultiplier == 0)
+            sizeMultiplier = 1;
+        else
+            StartCoroutine(IncreaseHealthBar(newMaxHp));
+
         healthSlider.maxValue = newMaxHp;
         if(healthSlider.maxValue < healthSlider.value) {
             healthSlider.value = newMaxHp;
@@ -52,5 +67,22 @@ public class PlayerHpController : MonoBehaviour
     /// </summary>
     private void PlayerDied() {
         gameObject.SetActive(false);
+    }
+
+    private IEnumerator IncreaseHealthBar(float newMaxHp)
+    {
+        float newSizeMultiplier;
+        if (healthSlider.maxValue < newMaxHp)
+            newSizeMultiplier = sizeMultiplier + (2 - sizeMultiplier) / 2.0f;
+        else
+            newSizeMultiplier = sizeMultiplier + (sizeMultiplier - 1) / 2.0f;
+
+        for (var i = 0.0f; i < 1.0f; i += 0.02f)
+        {
+            var val = maxHpChangeCurve.Evaluate(i);
+            float value = sizeMultiplier + ((newSizeMultiplier - sizeMultiplier) * val);
+            rect.sizeDelta = new Vector2(origSize * value, rect.rect.height);
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 }
