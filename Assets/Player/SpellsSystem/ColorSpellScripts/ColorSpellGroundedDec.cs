77af6104021620ee3da3bd75f821c25de0c68807
Markus <markus-grahn@hotmail.com>
2023-05-18 03:56:29 +0200

    Further balance and tweaks
    - Directly hitting an enemy damages the enemy as a normal color bolt but with 120% damage
    - The trap applies gravity when set which cases it to settle on the floor properly
    - Shortened the active window for the trap activating
    - Tweaked the spawning position so the spell does fly through walls when close to walls

--- Assets/Player/SpellsSystem/ColorSpellScripts/ColorSpellGroundedDec.cs
+++ Assets/Player/SpellsSystem/ColorSpellScripts/ColorSpellGroundedDec.cs
@@ -10,20 +10,27 @@ public class ColorSpellGroundedDec : SpellImpact
     /// <summary>
     /// Forwards the impact to the refer coponent
     /// </summary>
-    [SerializeField] SpellImpact refer;
+    [SerializeField] SpellImpact success;
+    [SerializeField] SpellImpact fail;
+    [SerializeField] float radius = 0;
 
     public override void Init(ColorSpell spell)
     {
         base.Init(spell);
-        refer.Init(spell);
+        success.Init(spell);
+        fail.Init(spell);
     }
 
     public override void Impact(Collider2D other)
     {
-        bool test = Physics2D.Raycast(transform.position , Vector2.down, 1f, ~LayerMask.GetMask("Enemy", "Player", "Spell", "Item"));
+        bool test = Physics2D.Raycast((Vector2) transform.position + Vector2.right*radius, Vector2.down, 1f, ~LayerMask.GetMask("Enemy", "Player", "Spell", "Item")) || 
+            Physics2D.Raycast((Vector2)transform.position - Vector2.right * radius, Vector2.down, 1f, ~LayerMask.GetMask("Enemy", "Player", "Spell", "Item"));
         if (test)
         {
-            refer.Impact(other);
+            success.Impact(other);
+        } else
+        {
+            fail.Impact(other);
         }
     }
 }
