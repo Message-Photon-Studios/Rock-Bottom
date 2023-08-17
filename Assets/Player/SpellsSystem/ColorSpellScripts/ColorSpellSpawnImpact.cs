4b4c250e4d2f80f7d72906e3fcbca5f1b7f83ad7
Markus <markus-grahn@hotmail.com>
2023-08-18 01:50:04 +0200

    Grape Spell Added

--- Assets/Player/SpellsSystem/ColorSpellScripts/ColorSpellSpawnImpact.cs
+++ Assets/Player/SpellsSystem/ColorSpellScripts/ColorSpellSpawnImpact.cs
@@ -5,51 +5,33 @@ using UnityEngine;
 /// <summary>
 /// Spawns other color spells at impact
 /// </summary>
-public class ColorSpellSpawn : SpellImpact
+public class ColorSpellSpawnImpact : SpellImpact
 {
     /// <summary>
     /// After this time the spell will autospawn the spawn spells.
     /// </summary>
-    [SerializeField] float spawnDelay;
-    private float delayTimer;
-    [SerializeField] int spawnAmount;
+    [SerializeField] float detonateTimer;
 
     /// <summary>
     /// The spells that will be spawned on impact
     /// </summary>
     [SerializeField] GameObject[] spawnPrefabs;
 
-    private void Start()
-    {
-        delayTimer = spawnDelay;
-
-    }
-
-
     private void Update() {
-        if (spawnAmount<=0) return;
-        delayTimer -= Time.deltaTime;
-        if (delayTimer <= 0)
+        detonateTimer -= Time.deltaTime;
+        if(detonateTimer <= 0)
         {
-            //Impact(null);
-            Spawn();
-            spawnAmount--;
-            delayTimer = spawnDelay;
-        }
-    }
-
-    public void Spawn()
-    {
-        foreach (GameObject spawnPrefab in spawnPrefabs)
-        {
-            GameObject obj = GameObject.Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
-            Debug.Log(spell.GetColor());
-            obj.GetComponent<ColorSpell>().Initi(spell.GetColor(), spell.GetPower(), spell.GetPlayerObj(), spell.lookDir);
+            Impact(null);
+            Destroy(gameObject);
         }
     }
     
     public override void Impact(Collider2D other)
     {
-
+        foreach (GameObject spawnPrefab in spawnPrefabs)
+        {
+            GameObject obj = GameObject.Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
+            obj.GetComponent<ColorSpell>().Initi(spell.GetColor(), spell.GetPower(), spell.GetPlayerObj(), spell.lookDir);
+        }
     }
 }
