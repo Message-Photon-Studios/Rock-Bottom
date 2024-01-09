dea78c81a803d2308948ae5584dde623551bbdff
EmilEkroth <emil@ekroth.se>
2024-01-09 20:33:42 +0100

    Fixed minor bugs

--- Assets/Enemies/EnemyBehaviours/SetEnemyColorToMine.cs
+++ Assets/Enemies/EnemyBehaviours/SetEnemyColorToMine.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BehaviourTree;
-using Unity.VisualScripting.Dependencies.Sqlite;
 
 public class SetEnemyColorToMine : Node
 {
