4a3d3849369e6cea21f032a666e1f800f968ffc4
EmilEkroth <emil@ekroth.se>
2024-08-26 12:55:24 +0300

    Updated gamemanger

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
