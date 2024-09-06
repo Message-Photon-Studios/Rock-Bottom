9974234e05fc2d24980217709f4d7eeb572bc7dc
EmilEkroth <emil@ekroth.se>
2024-09-06 12:14:59 +0300

    Updated gilded mines

--- Assets/Enemies/Scripts/EnemyStats.cs
+++ Assets/Enemies/Scripts/EnemyStats.cs
@@ -367,7 +367,7 @@ public class EnemyStats : MonoBehaviour
         GetComponent<Collider2D>().enabled = false;
         Destroy(gameObject, 5);
         SleepEnemy(10, 1, null);
-        int drainAmount = 3;
+        int drainAmount = 4;
         if(colorOrbPrefab != null && colorAmmount-drainAmount > 0 && color != null)
         {
             GameObject orb = Instantiate(colorOrbPrefab, GetPosition(), transform.rotation) as GameObject;
