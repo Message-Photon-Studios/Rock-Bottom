3cfc3c9542b4614c66d9e1860e904ee0f8052bf2
EmilEkroth <emil@ekroth.se>
2024-09-09 11:49:09 +0300

    Initial code for save-load system

---
+++ Assets/GameManager/DataPersistence/Data/GameData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+
+    /// <summary>
+    /// On new game this constructor will set default values.
+    /// </summary>
+    public GameData ()
+    {
+
+    }
+}
