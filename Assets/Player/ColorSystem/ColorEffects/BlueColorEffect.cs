8980ea2f739b8e6614d8ef3100ca129437a623ef
EmilEkroth <emil@ekroth.se>
2024-09-11 16:53:47 +0300

    Fixed blue

--- Assets/Player/ColorSystem/ColorEffects/BlueColorEffect.cs
+++ Assets/Player/ColorSystem/ColorEffects/BlueColorEffect.cs
@@ -20,15 +20,16 @@ public class BlueColorEffect : ColorEffect
 
         float effect = EffectFunction(power);
         float scaledDuration = duration * effect;
+        Debug.Log(scaledDuration);
         float scaledSlow = slow*effect;
         main.duration = scaledDuration;
-        Destroy(instantiatedParticles, scaledDuration*1.2f);
+        Destroy(instantiatedParticles, scaledDuration*1.1f);
         instantiatedParticles.GetComponent<ParticleSystem>().Play();
         // Set enemy as parent of the particle system
         instantiatedParticles.transform.parent = enemyObj.transform;
         if(!enemy.IsKnockbackImune())
             enemy.GetComponent<Rigidbody2D>()?.AddForce((enemy.transform.position-playerObj.transform.position).normalized * force);
-        enemy.ChangeDrag(scaledSlow+1, scaledDuration*power);
+        enemy.ChangeDrag(scaledSlow+1, scaledDuration);
         enemy.DamageEnemy(Mathf.RoundToInt(damage*power));
     }
 }
