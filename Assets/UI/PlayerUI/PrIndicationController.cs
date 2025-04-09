76177fd3e275a3ef30ccf9aaddbd7691acea8336
Markus <markus-grahn@hotmail.com>
2025-04-09 12:51:42 +0200

    Fixed some bugs with items
    centrifuge isnt always active
    concentraded color indicates properly

--- Assets/UI/PlayerUI/PrIndicationController.cs
+++ Assets/UI/PlayerUI/PrIndicationController.cs
@@ -51,7 +51,7 @@ public class PrIndicationController : MonoBehaviour
             border.color = new Color32(255, 255, 255, 150);
             
         } else {
-            float buff = 100*(colorInventory.GetColorBuff(currentColor) + colorInventory.GetColorSpell(slotIndex).powerScale);
+            float buff = 100*(colorInventory.GetColorBuff(currentColor) + colorInventory.GetColorSpell(slotIndex).powerScale + colorInventory.GetSlotBuff(slotIndex));
             buff = Mathf.Round(buff);
             if (buff <= 10) buff = 10;
             powerText.text = (int) buff + "%";
