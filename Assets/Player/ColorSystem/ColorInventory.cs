ca3530a408563a40abbe6675eedee1b212caa581
EmilEkroth <emil@ekroth.se>
2025-05-23 15:20:53 +0300

    fixed sharp brush

--- Assets/Player/ColorSystem/ColorInventory.cs
+++ Assets/Player/ColorSystem/ColorInventory.cs
@@ -63,6 +63,7 @@ public class ColorInventory : MonoBehaviour
     public bool greatBrushFirstHit = false;
     public bool paintersKnife = false;
     public bool colorVarnish = false;
+    public int rainbowComboExtraColor = 0;
     private float rngMax = 0;
     private float rngMin = 0;
     private float rngBuff = 0;
