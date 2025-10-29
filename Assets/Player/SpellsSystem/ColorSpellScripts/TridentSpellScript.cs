ac6fe3f45aac659c15919b1f5f46b12c4846f7e8
Markus <markus-grahn@hotmail.com>
2025-10-29 16:04:36 +0100

    Updated trident behaviour

--- Assets/Player/SpellsSystem/ColorSpellScripts/TridentSpellScript.cs
+++ Assets/Player/SpellsSystem/ColorSpellScripts/TridentSpellScript.cs
@@ -7,13 +7,20 @@ public class TridentSpellScript : SpellImpact
     [SerializeField] float strength;
     [SerializeField] LayerMask layersToHit;
     [SerializeField] float waitTime;
+    [SerializeField] float despawnDistance;
+    [SerializeField] float maxDistance;
+    [SerializeField] float canBoostAgianDistance;
     GameObject player;
     Rigidbody2D body;
     int count = 0;
     float time;
+    float timeSinceLastBoost;
+    float emmission = 20;
+    bool canBeDestroyed = false;
+    bool canBoostAgain = false;
+    bool justSpawned = true;
     public override void Impact(Collider2D other, Vector2 impactPoint)
     {
-        throw new System.NotImplementedException();
     }
 
     // Start is called before the first frame update
@@ -27,8 +34,21 @@ public class TridentSpellScript : SpellImpact
     // Update is called once per frame
     void Update()
     {
-        if (body.velocity.sqrMagnitude <= 0.5f * ((count + 10f) / 10f))
+        if (justSpawned)
         {
+            if (body.velocity.sqrMagnitude > 0.6f) justSpawned = false;
+            return;
+        }
+        float playerDistance = Vector2.Distance(player.transform.position, transform.position);
+        if (playerDistance <= despawnDistance && canBeDestroyed)
+        {
+            Destroy(gameObject);
+        }
+        if (playerDistance < canBoostAgianDistance) canBoostAgain = true;
+        if (body.velocity.sqrMagnitude <= 0.5f * ((count + 10f) / 10f) || playerDistance >= maxDistance && (timeSinceLastBoost <= Time.time || canBoostAgain))
+        {
+            body.velocity = new Vector2(0,0);
+            foreach (ParticleSystem particle in GetComponentsInChildren<ParticleSystem>()) particle.emissionRate = 0;
             if (count >= 20) Destroy(gameObject);
             Vector3 direction = player.transform.position - transform.position;
             float playerAngle = Mathf.Atan2(direction.y * spell.lookDir, direction.x * spell.lookDir) * Mathf.Rad2Deg ;
@@ -42,6 +62,10 @@ public class TridentSpellScript : SpellImpact
                     Vector2 dir = newDir * new Vector2(1 * spell.lookDir, 0);
                     body.velocity = (dir.normalized * 20 * (count + 10f)/10f);
                     count++;
+                    timeSinceLastBoost = Time.time + .5f;
+                    canBeDestroyed = true;
+                    canBoostAgain = false;
+                    foreach (ParticleSystem particle in GetComponentsInChildren<ParticleSystem>()) particle.emissionRate = emmission;
                 }
             }
         }
@@ -49,5 +73,10 @@ public class TridentSpellScript : SpellImpact
         {
             time = Time.time;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        body.velocity = new Vector2(0, 0);
     }
 }
