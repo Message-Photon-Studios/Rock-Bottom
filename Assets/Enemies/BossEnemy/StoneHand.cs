294bc46147e96a960347eaa920e195c0a2a9c488
EmilEkroth <emil@ekroth.se>
2026-02-17 15:55:50 +0200

    Updated quasar stone attack

---
+++ Assets/Enemies/BossEnemy/StoneHand.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourTree;
+using UnityEditor;
+using UnityEngine.Events;
+
+public class StoneHand : Enemy
+{
+    [SerializeField] GameObject stoneProjectile;
+    [SerializeField] float stoneUpForce;
+    [SerializeField] float stoneRadomForce;
+    [SerializeField] float stonesHpSpawn;
+    [SerializeField] Vector2 spawnOffset;
+
+        
+    protected override Node SetupTree()
+    {
+        Node root = new Sequence(new List<Node>
+        {
+            new CheckBool("stoneThrowAttack", true),
+            new EnemyObjectSpawner(stats, stoneProjectile, spawnOffset, Vector2.up*stoneUpForce, false, stoneRadomForce),
+            new SetParentVariable("stoneThrowAttack", false, 1),
+        });
+
+        root.SetData("stoneThrowAttack", false);
+        return root;
+    }
+}
